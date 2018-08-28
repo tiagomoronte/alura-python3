2c7556175ba188feff6a862bc91294279ccecf3f
Tiago Moronte <tiago.moronte@gmail.com>
2018-08-28 11:42:35 -0300

    Aula 7

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -1,10 +1,23 @@
+import random
+
 print("********************************")
 print("Bem vindo ao jogo de adivinhação")
 print("********************************")
 
-numero_secreto = 42
-total_de_tentativas = 3
-acertou = False
+numero_secreto = int(random.randrange(1, 101)) # entre 0.0 e 1.0
+total_de_tentativas = 0
+pontos = 1000
+
+print("Qual o níevel de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("Defina o nível: "))
+if(nivel == 1):
+    total_de_tentativas = 20
+elif(nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
 
 for rodada in range(1, total_de_tentativas + 1):
 
@@ -29,7 +42,10 @@ for rodada in range(1, total_de_tentativas + 1):
             print("Chute maior")
         elif (menor):
             print("Chute menor")
+        pontos = pontos - (abs(numero_secreto - chute))
 
     rodada += 1
 
+print("Pontos {}".format(pontos))
+print (numero_secreto)
 print("Fim do jogo.")
