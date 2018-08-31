6766d4a9c27d536810c26fb1c08c14a56e9c638f
Tiago Moronte <tiago.moronte@gmail.com>
2018-08-31 17:56:28 -0300

    Aula 3

--- jogos/forca.py
+++ jogos/forca.py
@@ -5,23 +5,36 @@ def jogar():
     print("** Bem vindo ao jogo de forca! **")
     print("*********************************")
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_","_","_","_","_","_"]
+    palavra_secreta = "python".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
+    erros = 0
 
     print(letras_acertadas)
 
     while(not enforcou and not acertou):
-        chute = input("Qual letra?").strip()
+        chute = input("Qual letra?").strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if (letra.upper() == chute.upper()):
-                letras_acertadas[index] = letra
-            index += 1
+        if (chute in palavra_secreta):
+
+            index = 0
+            for letra in palavra_secreta:
+                if (letra == chute):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
+
+    if (acertou):
+        print("Parabéns!")
+    else:
+        print("Perdeu")
 
     print("Fim do jogo.")
 
